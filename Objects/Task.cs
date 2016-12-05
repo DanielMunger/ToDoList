85842a64c45ae361da67ffd9e8449fa3d3f35ea1
Daniel Munger and Erik Killops <daniel+erik@>
2016-12-05 11:48:12 -0800

    add copypasta.

---
+++ Objects/Task.cs
@@ -0,0 +1,108 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace ToDoList
+{
+  public class Task
+  {
+    private int _id;
+    private string _description;
+
+    public Task(string Description, int Id = 0)
+    {
+      _id = Id;
+      _description = Description;
+    }
+
+    public override bool Equals(System.Object otherTask)
+    {
+      if (!(otherTask is Task))
+      {
+        return false;
+      }
+      else
+      {
+        Task newTask = (Task) otherTask;
+        bool idEquality = (this.GetId() == newTask.GetId());
+        bool descriptionEquality = (this.GetDescription() == newTask.GetDescription());
+        return (idEquality && descriptionEquality);
+      }
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("Insert INTO tasks (description) OUT INSERTED.id VALUES (@TaskDescription);", conn);
+
+      SqlParameter descriptionParameter = new SqlParameter();
+      descriptionParameter.ParameterName = "@TaskDescription";
+      descriptionParameter.Value = this.GetDescription();
+      cmd.Parameters.Add(descriptionParameter);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if(conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetDescription()
+    {
+     return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public static List<Task> GetAll()
+    {
+      List<Task> allTasks = new List<Task> {};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT *  FROM tasks;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int taskID = rdr.GetInt32(0);
+        string taskDescription = rdr.GetString(1);
+        Task newTask = new Task(taskDescription, taskId);
+        allTasks.Add(newTask);
+      }
+      if(rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allTasks;
+     }
+     public static void DeleteAll()
+     {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM tasks;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+     }
+   }
+}
